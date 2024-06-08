779ba20412c5462f06c14a9fb98881fdc2f0fd9f
Felipe Carmona <felipecarmonamaturana@gmail.com>
2024-06-07 21:59:31 -0400

    binding-vue_tarjeta-credito-v01

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+      <form>
+        <div>
+          <label>Título de la tarjeta: </label>
+          <input v-model="tituloTarjeta"/>
+        </div>
+
+        <div>
+          <label>Chip SRC: </label>
+          <input v-model="imagenChipTarjeta"/>
+        </div>
+
+        <div>
+          <label>Número: </label>
+          <input v-model="numeroTarjeta"/>
+        </div>
+
+        <div>
+          <label>Fecha de expiración: </label>
+          <input v-model="fechaExpTarjeta"/>
+        </div>
+
+        <div>
+          <label>Propietario: </label>
+          <input v-model="propietarioTarjeta"/>
+        </div>
+
+        <div>
+          <label>Tipo de tarjeta SRC: </label>
+          <input v-model="imagenTarjeta"/>
+        </div>
+      </form>
+
+      <div class="carnet">
+        <h3>{{ tituloTarjeta }}</h3>
+        <img width="40" :src="imagenChipTarjeta" alt="chip" />
+        <div>
+          <h2>{{ numeroTarjeta }}</h2>
+          <span>Fecha Exp: <b>{{fechaExpTarjeta}}</b></span>
+        </div>
+        <footer>
+          <span>{{propietarioTarjeta}}</span>
+          <img :src="imagenTarjeta" width="60" alt ="logo tarjeta" />
+        </footer>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      tituloTarjeta:"",
+      imagenChipTarjeta:"",
+      imagenTarjeta:"",
+      numeroTarjeta:"",
+      fechaExpTarjeta:"",
+      propietarioTarjeta:""
+    }
   }
 }
 </script>
@@ -23,4 +74,80 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+* {
+        margin: 0;
+      }
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 60vh;
+        font-size: 10px;
+        font-family: sans-serif;
+      }
+
+      #app {
+        display: flex;
+        gap: 40px;
+      }
+      form {
+        padding: 18px;
+        border-radius: 20px;
+        border: ridge 1px;
+        box-shadow: 1px 1px 1px 1px gray;
+        font-weight: bold;
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        justify-content: center;
+      }
+      form div {
+        display: grid;
+        grid-template-columns: 100px 200px;
+        align-items: center;
+        gap: 5px;
+      }
+
+      form div label {
+        text-align: right;
+      }
+
+      .carnet {
+        width: 400px;
+        height: 230px;
+        background: #1c1a1e;
+        color: white;
+        border-radius: 20px;
+        padding: 20px;
+        box-shadow: 0px 0px 10px 2px black;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+
+      h3 {
+        font-size: 20px;
+      }
+
+      h2 {
+        font-size: 30px;
+        letter-spacing: 10px;
+        margin-bottom: 15px;
+        text-shadow: 0px 0px 1px gold;
+        font-family: Times;
+      }
+
+      b {
+        font-size: 16px;
+      }
+
+      footer {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        font-weight: bold;
+        font-size: 18px;
+        text-transform: uppercase;
+        font-style: italic;
+      }
 </style>
